bfed91343875fed5e8ac5c3ce8bc5271cb515ed1
UmetalievIskender <iskender.umetaliev@student.hogent.be>
2020-03-25 02:08:30 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/UmetalievIskender/drmon/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/UmetalievIskender/drmon/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
